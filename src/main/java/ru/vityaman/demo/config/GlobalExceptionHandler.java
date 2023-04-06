43e37a002183271d8c8731889d80babe43a79a63
vityaman <vityaman.dev@yandex.ru>
2023-04-06 18:53:53 +0300

    [messages] added api spec and delegate

--- src/main/java/ru/vityaman/demo/config/GlobalExceptionHandler.java
+++ src/main/java/ru/vityaman/demo/config/GlobalExceptionHandler.java
@@ -6,21 +6,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import ru.vityaman.demo.api.model.GeneralErrorView;
+
 import java.io.IOException;
-import java.util.Map;
+
+import ru.vityaman.demo.api.model.GeneralErrorView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<GeneralErrorView> handleException(
-            ResponseStatusException e
-    ) throws IOException {
+            ResponseStatusException e) throws IOException {
         return new ResponseEntity<>(new GeneralErrorView()
-            .code(e.getRawStatusCode())
-            .status(e.getStatus().toString())
-            .message(e.getReason()),
-            HttpStatus.valueOf(e.getRawStatusCode())
-        );
+                .code(e.getRawStatusCode())
+                .status(e.getStatus().toString())
+                .message(e.getReason()),
+                HttpStatus.valueOf(e.getRawStatusCode()));
     }
 }
